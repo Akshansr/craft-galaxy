4ee19168f3d16e5d37a0b977667c7c64a225081d
akshasr <srujan.akshanthala@gmail.com>
2021-05-14 02:06:45 +0530

    'changes-production-build'

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Traditional Home Made Christmas Decoration',
+    image: '/images/christmasDecoration.jpg',
+    description:
+      'If your budget is tight for the Christmas and doesn’t want to spend the money on the decoration. Then you must be looking for a kind of decoration theme which is cheap and can be made by the things available in the home. Paper is a cheap and easily available material in the home. So why not give a try to make the Paper Christmas Decorations for this year',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Dream Catcher at Home',
+    image: '/images/dreamCatcher.jpg',
+    description:
+      'Gold-Toned & Brown Coffee Duo Decorative Dream Catcher',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 59.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Handemade Frame',
+    image: '/images/handmadeFrame.jpg',
+    description:
+      'A picture frame protects the art and presents it to good advantage',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 32.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Hand Made Wooden Craft',
+    image: '/images/handmadeWoodCraft.jpg',
+    description:
+      'A perfect gift for your Anniversary or Valentines Day.Our blocks are designed and made by hand in our Vermont workshop from local hardwood.',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 39.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Photo Stand',
+    image: '/images/photoStand.jpg',
+    description:
+      'Photo craft is a timeless tradition for DIY home decor! Considering making this personalized photo frame wreath with the following instructions via source. DIY ',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 4.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Wall Mount',
+    image: '/images/wallMountStand.jpg',
+    description:
+      'Mount it to the kitchen, office wall or entryway, wherever there is easy access',
+    brand: 'HomeMade',
+    category: 'HomeMade',
+    price: 9.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
